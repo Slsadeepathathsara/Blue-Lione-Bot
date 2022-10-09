522d1b48d68856852babe879cd3e1d955643e016
Sadeepa <115361661+Slsadeepathathsara@users.noreply.github.com>
2022-10-09 07:04:14 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -81,7 +81,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['94714188730'] //ur premium numbers
+global.premium = ['94720841518'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
